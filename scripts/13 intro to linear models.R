f7075b101b9f6fbf8c61ebbc6d75d119072810dc
Zedhadouken <kzz20wvu@uea.ac.uk>
2023-03-03 13:38:06 +0000

    Halfway through chapter 13

--- scripts/13 intro to linear models.R
+++ scripts/13 intro to linear models.R
@@ -13,3 +13,34 @@ broom::tidy(lsmodel0)
 
 #proving that the intercept is the same as the overall mean
 mean(darwin$height)
+
+#compare means----
+lsmodel1 <- lm(height ~ type, data=darwin) # note that the following is identical
+                                           # lsmodel1 <- lm(height ~ 1 + type, data=darwin)
+broom::tidy(lsmodel1)
+
+summary(lsmodel1) #a look at the full summary
+
+darwin %>%                                 # superimpose the calculated means onto a plot.
+  ggplot(aes(x=type,
+             y=height,
+             colour=type))+
+  geom_jitter(alpha=0.5,
+              width=0.1)+
+  stat_summary(fun=mean,
+               size=1.2)+
+  theme_bw()
+
+#standard error of the difference----
+## second row gives the mean difference in this column it gives the standard error of the difference between the two means (SED)
+
+#confidence intervals----
+broom::tidy(lsmodel1, conf.int=T)
+
+#answering the question----
+GGally::ggcoef_model(lsmodel1,
+                     show_p_values=FALSE, 
+                     conf.level=0.95)
+
+broom::tidy(lsmodel1, conf.int=T, conf.level=0.99) # changing [increasing] the confidence level will mean you no longer are able to reject the null hypthesis
+
